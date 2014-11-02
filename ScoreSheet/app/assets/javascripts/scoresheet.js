cb9ba520a4aa9eaeb1c65484df1e28e78708943c
H.Tajima <tajihiro2@gmail.com>
2014-11-03 00:10:23 +0900

    Bootstrapレイアウト作成

---
+++ ScoreSheet/app/assets/javascripts/scoresheet.js
@@ -0,0 +1,3 @@
+$(function() {
+    $( "#datepicker" ).datepicker().datepicker("option", "dateFormat","yy-mm-dd");
+});
--- ScoreSheet/config/routes.rb
+++ ScoreSheet/config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+
+  root 'score_sheet#index'
+  get 'score_sheet/index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
