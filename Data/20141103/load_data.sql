813e59e6ed6ad00c229d568830ec91d5679f1384
H.Tajima <tajihiro2@gmail.com>
2014-11-03 18:00:30 +0900

    データ加工

---
+++ Data/20141103/load_data.sql
@@ -0,0 +1,8 @@
+load data local infile "prefectures.csv" into table prefectures fields terminated by "," enclosed by '"' lines terminated by '\n';
+load data local infile "divisions.csv" into table divisions fields terminated by "," enclosed by '"' lines terminated by '\n';
+load data local infile "seasons.csv" into table seasons fields terminated by "," enclosed by '"' lines terminated by '\n';
+load data local infile "positions.csv" into table positions fields terminated by "," enclosed by '"' lines terminated by '\n';
+load data local infile "teams.csv" into table teams fields terminated by "," enclosed by '"' lines terminated by '\n';
+load data local infile "members.csv" into table members fields terminated by "," enclosed by '"' lines terminated by '\n';
+
+load data local infile "s.csv" into table s fields terminated by "," enclosed by '"' lines terminated by '\n';
