78f7afd1614cb5a1a1d094f821e09acc20cfb870
H.Tajima <tajihiro2@gmail.com>
2014-11-18 12:02:49 +0900

    Modified data.

---
+++ Data/20141115/load_data.sql
@@ -0,0 +1,12 @@
+load data local infile "prefectures.csv" into table prefectures fields terminated by "," enclosed by '"' lines terminated by '\n';
+load data local infile "divisions.csv" into table divisions fields terminated by "," enclosed by '"' lines terminated by '\n';
+load data local infile "seasons.csv" into table seasons fields terminated by "," enclosed by '"' lines terminated by '\n';
+load data local infile "positions.csv" into table positions fields terminated by "," enclosed by '"' lines terminated by '\n';
+load data local infile "teams.csv" into table teams fields terminated by "," enclosed by '"' lines terminated by '\n';
+load data local infile "members.csv" into table members fields terminated by "," enclosed by '"' lines terminated by '\n';
+load data local infile "games.csv" into table games fields terminated by "," enclosed by '"' lines terminated by '\n';
+load data local infile "season_teams.csv" into table season_teams fields terminated by "," enclosed by '"' lines terminated by '\n';
+load data local infile "season_members.csv" into table season_members fields terminated by "," enclosed by '"' lines terminated by '\n';
+load data local infile "game_members.csv" into table game_members fields terminated by "," enclosed by '"' lines terminated by '\n';
+load data local infile "goals.csv" into table goals fields terminated by "," enclosed by '"' lines terminated by '\n';
+load data local infile "penalties.csv" into table penalties fields terminated by "," enclosed by '"' lines terminated by '\n';
