791674950a9589fd821def6baf68aabd6511c49b
H.Tajima <tajihiro2@gmail.com>
2015-01-03 10:13:25 +0900

    modified deploy.rb.

--- ScoreSheet/config/deploy.rb
+++ ScoreSheet/config/deploy.rb
@@ -23,7 +23,7 @@ set :scm, :git
 # set :pty, true
 
 # Default value for :linked_files is []
-set :linked_files, fetch(:linked_files, []).push('config/database.yml')
+#set :linked_files, fetch(:linked_files, []).push('config/database.yml')
 #set :linked_files, %w{config/database.yml}
 
 # Default value for linked_dirs is []
